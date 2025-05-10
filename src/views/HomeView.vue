76d9643b0e9b7c14c53255202c376edd515c38b2
Nielsen <asz718460@gmail.com>
2025-05-10 10:48:46 +0800

    init: 專案初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useCounterStore } from '../stores/counter'
+
+const counter = useCounterStore()
+</script>
+
+<template>
+  <main>
+    <h1>Welcome to the Home Page</h1>
+    <p>Count: {{ counter.count }}</p>
+    <p>Double Count: {{ counter.doubleCount }}</p>
+    <button @click="counter.increment">Increment</button>
+  </main>
+</template>
+
+<style lang="scss"></style>
