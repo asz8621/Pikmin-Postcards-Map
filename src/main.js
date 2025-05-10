666c8116630038dd7dcec938ac5797bf9768fd11
Nielsen <asz718460@gmail.com>
2025-05-10 11:30:03 +0800

    feat: 新增 leaflet 與 markercluster

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { naive } from './plugins/naive-ui'
+import 'leaflet/dist/leaflet.css' // 引入 Leaflet 樣式
+import 'leaflet.markercluster/dist/MarkerCluster.css' // 引入 MarkerCluster 樣式
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css' // 引入 MarkerCluster 預設樣式
 
 const app = createApp(App)
 
