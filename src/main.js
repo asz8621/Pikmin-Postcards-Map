3f576a0d2a2e0a1ba61992ec6afa4dca7f5d87b8
Nielsen <asz718460@gmail.com>
2025-05-12 11:04:52 +0800

    feat: 在 main.js 中導入全域 base.css 樣式

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { naive } from './plugins/naive-ui'
 import 'leaflet/dist/leaflet.css' // 引入 Leaflet 樣式
 import 'leaflet.markercluster/dist/MarkerCluster.css' // 引入 MarkerCluster 樣式
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css' // 引入 MarkerCluster 預設樣式
+import '@/assets/css/base.css'
 
 const app = createApp(App)
 
