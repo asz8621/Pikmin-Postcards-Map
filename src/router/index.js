35c6456159dd45788b0bc9cbae29767f36eae6da
Nielsen <asz718460@gmail.com>
2025-05-13 10:19:03 +0800

    feat: 新增 login 與 map 路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Cookies from 'js-cookie'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/login',
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/LoginView.vue'),
+  },
+  {
+    path: '/map',
+    name: 'map',
+    component: () => import('@/views/MapView.vue'),
+    meta: { requiresAuth: true },
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue'),
-    },
-  ],
+  routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const token = Cookies.get('token')
+  if (to.meta.requiresAuth && !token) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
