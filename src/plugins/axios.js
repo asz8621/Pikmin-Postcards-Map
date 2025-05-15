3b87fe0e269e23bc1bf000896e0b6dd0a93b0a29
Nielsen <asz718460@gmail.com>
2025-05-15 15:08:18 +0800

    feat: axios 統一處理 401 錯誤並調整 headers 設定

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,13 +1,11 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
+import router from '@/router'
 const baseURL = import.meta.env.VITE_API_BASE_URL
 
 const instance = axios.create({
   baseURL,
   timeout: 10000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 })
 
 instance.interceptors.request.use(
@@ -19,6 +17,17 @@ instance.interceptors.request.use(
     return config
   },
   (error) => Promise.reject(error),
+)
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      Cookies.remove('token')
+      router.push('/login')
+    }
+    return Promise.reject(error)
+  },
 )
 
 export default instance
