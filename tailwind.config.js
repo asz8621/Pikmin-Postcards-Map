2ba2bf8dd64ce38edac38e3f71b7d0116591d86a
Nielsen <asz718460@gmail.com>
2025-05-18 23:45:16 +0800

    fix: 修正 tailwind.config.js 的 screens 設定錯誤

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {
+    screens: {
       xs: '480px',
       sm: '576px',
       md: '768px',
       lg: '1024px',
       xl: '1280px',
       '2xl': '1536px',
+    },
+    extend: {
       backgroundImage: {
         login: "url('@/assets/images/login_bg.png')",
       },
