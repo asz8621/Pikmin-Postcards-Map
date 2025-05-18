8e70943061b635f22c8aa2a9982707e14edfcce2
Nielsen <asz718460@gmail.com>
2025-05-18 16:07:18 +0800

    refactor: LoginView.vue 改用 Tailwind CSS 樣式

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -13,16 +13,17 @@ const loginData = ref({
   account: '',
   password: '',
 })
+
 const loading = ref(false)
 
-const formRef = ref(null)
+const loginFormRef = ref(null)
 const rules = {
   account: [{ required: true, message: '請輸入帳號', trigger: 'blur' }],
   password: [{ required: true, message: '請輸入密碼', trigger: 'blur' }],
 }
 
 const login = async () => {
-  await formRef.value?.validate()
+  await loginFormRef.value?.validate()
   loading.value = true
   try {
     const res = await axios.post('/user/login', {
@@ -31,14 +32,14 @@ const login = async () => {
     })
     const token = res.data.data.token
     if (token) {
-      Cookies.set('token', token, { expires: 7 })
+      Cookies.set('token', token, { expires: 1 })
       successMsg(res.data.message)
       router.push('/map')
     } else {
       errorMsg('登入失敗：無法驗證用戶')
     }
   } catch (err) {
-    errorMsg(err.response?.data?.message || '登入錯誤')
+    errorMsg(err.response?.data?.message || '登入錯誤，請聯絡管理員')
   } finally {
     loading.value = false
   }
@@ -47,35 +48,40 @@ const login = async () => {
 
 <template>
   <div
-    style="
-      max-width: 400px;
-      margin: 100px auto;
-      padding: 20px;
-      border: 1px solid #eee;
-      border-radius: 8px;
-    "
+    class="flex justify-center items-start min-h-screen bg-login bg-center bg-center p-4 sm:p-0 sm:items-center"
   >
-    <h1>登入頁面</h1>
-
-    <n-form
-      ref="formRef"
-      :model="loginData"
-      :rules="rules"
-      :disabled="loading"
-      :show-require-mark="false"
+    <div
+      class="w-full sm:w-[400px] p-6 bg-green-50/90 rounded-lg shadow-lg mt-[100px] sm:mt-0 transition-width duration-500 ease-in-out"
     >
-      <n-form-item label="帳號" path="account">
-        <n-input v-model:value="loginData.account" placeholder="請輸入帳號" />
-      </n-form-item>
-      <n-form-item label="密碼" path="password">
-        <n-input
-          v-model:value="loginData.password"
-          type="password"
-          placeholder="請輸入密碼"
-          show-password-on="click"
-        />
-      </n-form-item>
-      <n-button type="primary" block :loading="loading" @click="login"> 登入 </n-button>
-    </n-form>
+      <h1 class="text-3xl font-semibold text-center text-green-600 mb-6">
+        <img src="@/assets/images/logo.png" alt="logo" class="mx-auto" />
+      </h1>
+
+      <n-form
+        ref="loginFormRef"
+        :model="loginData"
+        :rules="rules"
+        :disabled="loading"
+        :show-require-mark="false"
+        @keydown.enter.prevent="login"
+      >
+        <n-form-item label="帳號" path="account">
+          <n-input v-model:value="loginData.account" placeholder="請輸入帳號" />
+        </n-form-item>
+        <n-form-item label="密碼" path="password">
+          <n-input
+            v-model:value="loginData.password"
+            type="password"
+            placeholder="請輸入密碼"
+            show-password-on="click"
+          />
+        </n-form-item>
+        <n-button type="primary" block :loading="loading" :disabled="loading" @click="login">
+          登入
+        </n-button>
+      </n-form>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      xs: '480px',
+      sm: '576px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+      backgroundImage: {
+        login: "url('@/assets/images/login_bg.png')",
+      },
+    },
   },
   plugins: [],
 }
